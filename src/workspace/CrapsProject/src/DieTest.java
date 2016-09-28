d289ad990d8cba36ee4480495dda77cddfaa28ed
“Archit <190902@hkis.edu.hk>
2016-09-28 08:51:33 +0800

    XD
    XD

---
+++ src/workspace/CrapsProject/src/DieTest.java
@@ -0,0 +1,10 @@
+package workspace.CrapsProject.src;
+
+public class DieTest {
+public static void main(String[] args){
+	Die sausage = new Die();
+	System.out.println(sausage.getNumDots());
+	sausage.roll();
+	
+	}
+}
